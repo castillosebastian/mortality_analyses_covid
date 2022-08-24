7dc311c8697f094ed35641b5142524fa5ad07883
sebacastillo <castilloclaudiosebastian@gmail.com>
2022-08-24 13:07:45 -0300

    primera subida

---
+++ src/config/constants.R
@@ -0,0 +1,14 @@
+HOME_DIR = "~/R/mortality_analyses_covid"
+
+RAW_DATA_DIR = paste0(HOME_DIR,  "/data/raw")
+PROCESSED_DATA_DIR = paste0(HOME_DIR,  "/data/processed")
+FEATURES_DATA_DIR = paste0(HOME_DIR,  "/data/features")
+
+LOG_DIR = paste0(HOME_DIR,  "/logs")
+SUBMISSION_DIR = paste0(HOME_DIR,  "/submissions")
+OOF_DIR = paste0(HOME_DIR,  "/oof")
+FI_DIR = paste0(HOME_DIR,  "/fi")
+FI_FIG_DIR = paste0(HOME_DIR,  "/fi_fig")
+HPO_DIR = paste0(HOME_DIR,  "/hpo")
+
+TRACKING_FILE = paste0(HOME_DIR,  "/tracking/tracking.csv")
---
+++ src/scripts/data_processing/process_raw_data.R
@@ -0,0 +1,5 @@
+## get raw data, changes the data types, save to data process files.
+## final output is written to the specified directory
+
+source("src/common/com_util.R")
+source("src/config/constants.R")
